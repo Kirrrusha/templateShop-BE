fad63336531a71833962fd420388dae3c8e70823
kirrrusha <kirilllebedenko@gmail.com>
2020-02-13 11:28:29 +0300

    init https

--- src/initializers/server.js
+++ src/initializers/server.js
@@ -13,45 +13,6 @@ const swaggerConfig = require('../swagger');
 const passportConfig = require('../config/passport');
 
 const app = express();
-// app.server = http.createServer(app);
-
-// HTTP request logger
-if (process.env.NODE_ENV !== 'production') {
-  app.use(logger.devLogger);
-}
-
-app.use(logger.errorLogger);
-
-passportConfig(passport);
-
-// 3rd party middleware
-app.use(cors({
-  exposedHeaders: config.corsHeaders,
-}));
-
-app.use(bodyParser.json({
-  limit: config.bodyLimit,
-}));
-
-// compression
-app.use(compression());
-
-// swagger
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
-
-// api router
-app.use('/api', routes);
-
-app.use((err, req, res, next) => {
-  if (err) {
-    req.err = err.stack;
-    finalhandler(req, res)(err);
-    console.error(err);
-    return;
-  }
-
-  next();
-});
 
 function run() {
   app.listen(config.port, () => {
