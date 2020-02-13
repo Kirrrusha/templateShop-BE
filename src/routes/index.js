cb84b2061f207283cdd21431bf09a26aaa240ef0
kirrrusha <kirilllebedenko@gmail.com>
2020-02-13 11:42:39 +0300

    init

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const { version } = require('../../package.json');
-const router = require('express').Router();
-const v1 = require('./v1');
-
-router.use('/v1', v1);
-
-router.get('/', (req, res) => res.json({ version }));
-
-module.exports = router;
