31c2112c730b137a913b0e627b744fd62b5d9800
kirrrusha <kirilllebedenko@gmail.com>
2020-02-14 22:58:32 +0300

    routes, cors, compression

--- src/initializers/server.js
+++ src/initializers/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const {port} = require('../config');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cors = require('cors');
+const {port, corsHeaders, bodyLimit} = require('../config');
+const routes = require('../routes');
 
 const app = express();
 
@@ -7,6 +11,18 @@ const app = express();
 app.get('/', (req, res) => {
   res.json({message: 'ok'})
 });
+
+app.use(cors({
+  exposedHeaders: corsHeaders
+}));
+
+app.use(bodyParser.json({
+  limit: bodyLimit,
+}));
+
+app.use(compression())
+
+app.use('/api', routes);
 
 function run() {
   app.listen(port, () => {
