386231c4bc265b6ed7b341bcc967a109b55a0984
kirrrusha <kirilllebedenko@gmail.com>
2020-02-26 22:46:54 +0300

    add attributes API

--- src/routes/v1/products/index.js
+++ src/routes/v1/products/index.js
@@ -3,8 +3,10 @@ const options = require('./options');
 const groupAttributes = require('./group-attributes');
 const attributes = require('./attributes');
 
+router.get('/', (req, res) => res.json({ message: 'version' }));
 router.use('/options', options);
 router.use('/group-attributes', groupAttributes);
+
 router.use('/attributes', attributes);
 
 module.exports = router;
