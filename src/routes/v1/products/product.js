73e645c4229ad8cd068a72cb46084bf25568c11b
kirrrusha <kirilllebedenko@gmail.com>
2020-04-28 22:29:53 +0300

    fix check image product

--- src/routes/v1/products/product.js
+++ src/routes/v1/products/product.js
@@ -10,25 +10,35 @@ const { validate } = require('../../../middleware');
 
 
 const ordersValidator = [
-  check('name').not().isEmpty()
+  check('name')
+    .not()
+    .isEmpty()
     .withMessage('Obligatory field')
-    .isLength({max: 15, min: 2})
+    .isLength({
+      max: 15,
+      min: 2
+    })
     .withMessage('Wrong length name')
     .trim()
     .escape(),
-  check('description').optional()
-    .isLength({max: 1000})
+  check('description')
+    .optional()
+    .isLength({ max: 1000 })
     .withMessage('Too much long')
     .trim()
     .escape(),
-  check('status').optional()
+  check('status')
+    .optional()
     .isBoolean()
     .withMessage('Wrong type'),
-  check('deductFromStock').optional()
+  check('deductFromStock')
+    .optional()
     .isBoolean()
     .withMessage('Wrong type'),
-  check('manufactureId').optional()
-    .isNumeric().withMessage('Wrong type')
+  check('manufactureId')
+    .optional()
+    .isNumeric()
+    .withMessage('Wrong type')
 ];
 
 
@@ -48,33 +58,36 @@ const upload = multer({
   },
   fileFilter: async (req, file, cb) => {
     if (file.length > 12) {
-      cb(new Error("Too many files"), false);
-    } else if (
-      file.mimetype === "image/png" ||
-      file.mimetype === "image/jpg" ||
-      file.mimetype === "image/jpeg"
+      cb(new Error('Too many files'), false);
+    }
+    if (
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg'
     ) {
-      cb(null, true);
-    } else if (req.method === 'POST') {
-      try {
-        const product = await Product.findOne({ name: req.body.name }).exec();
-        console.log('product fileFilter POST', product)
-        if (!isEmpty(product)) {
-          cb(new Error('Product already exist'), false);
+      if (req.method === 'POST') {
+        try {
+          const product = await Product.findOne({ name: req.body.name })
+            .exec();
+          console.log('product fileFilter POST', product);
+          if (!isEmpty(product)) {
+            cb(new Error('Product already exist'), false);
+          }
+          cb(null, true);
+        } catch (e) {
+          cb(null, false, new Error(e));
+        }
+      } else if (req.method === 'PUT') {
+        const product = await Product.findById(req.body.id)
+          .exec();
+        console.log('product fileFilter PUT', product);
+        if (!product) {
+          cb(new Error('Product not found'), false);
         }
         cb(null, true);
-      } catch (e) {
-        cb(null, false, new Error(e));
       }
-    } else if (req.method === 'PUT') {
-      const product = await Product.findById(req.body.id).exec();
-      console.log('product fileFilter PUT', product);
-      if (!product) {
-        cb(new Error('Product not found'), false);
-      }
-      cb(null, true);
     } else {
-      cb(new Error("File format should be PNG,JPG,JPEG"), false);
+      cb(new Error('File format should be PNG,JPG,JPEG'), false);
     }
   }
 });
